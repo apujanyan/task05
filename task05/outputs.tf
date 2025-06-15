4418421b10fcd04e4cbd9269cb1244d1cb1d20b7
Aramazd Apujanyan <apujanyan04@gmail.com>
2025-06-15 18:08:03 +0400

    ...

--- task05/outputs.tf
+++ task05/outputs.tf
@@ -1,3 +1,4 @@
 output "traffic_manager_fqdn" {
-  value = module.traffic_manager.fqdn
+  description = "The fully qualified domain name (FQDN) of the Azure Traffic Manager profile."
+  value       = module.traffic_manager.this.dns_config[0].fqdn
 }
